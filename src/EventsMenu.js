42dc553d34007b25c8d774627b4b667fbda4601c
Nithish <nithishprabhu123@gmail.com>
2022-05-01 19:42:02 +0530

    Need to sort out Horizontal scroll

--- src/EventsMenu.js
+++ src/EventsMenu.js
@@ -1,17 +1,91 @@
+import React from 'react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
-import HorizontalSlider from 'react-horizontal-slider';
+const getItems = () =>
+  Array(20)
+    .fill(0)
+    .map((_, ind) => ({ id: `element-${ind}` }));
 
-const EventsMenu = ({events,title}) => {
+function EventsMenu(events,title) {
+  const [items, setItems] = React.useState(getItems);
+  const [selected, setSelected] = React.useState([]);
+  const [position, setPosition] = React.useState(0);
 
-    return (
-            <HorizontalSlider 
-                title={title}
-                data={events}
-                height={300}
-                width={300}
-                id={'1'}
-            />
-    );
+  const isItemSelected = (id) => !!selected.find((el) => el === id);
+
+  const handleClick =
+    (id) =>
+    ({ getItemById, scrollToItem }) => {
+      const itemSelected = isItemSelected(id);
+
+      setSelected((currentSelected) =>
+        itemSelected
+          ? currentSelected.filter((el) => el !== id)
+          : currentSelected.concat(id)
+      );
+    };
+
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {items.map(({ id }) => (
+        <Card
+          itemId={id} // NOTE: itemId is required for track items
+          title={id}
+          key={id}
+          onClick={handleClick(id)}
+          selected={isItemSelected(id)}
+        />
+      ))}
+    </ScrollMenu>
+  );
 }
+
+function LeftArrow() {
+  const { isFirstItemVisible, scrollPrev } =
+    React.useContext(VisibilityContext);
+
+  return (
+    <Arrow disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+      Left
+    </Arrow>
+  );
+}
+
+function RightArrow() {
+  const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
+
+  return (
+    <Arrow disabled={isLastItemVisible} onClick={() => scrollNext()}>
+      Right
+    </Arrow>
+  );
+}
+
+function Card({ onClick, selected, title, itemId }) {
+  const visibility = React.useContext(VisibilityContext);
+
+  return (
+    <div
+      onClick={() => onClick(visibility)}
+      style={{
+        width: '160px',
+      }}
+      tabIndex={0}
+    >
+      <div className="card">
+        <div>{title}</div>
+        <div>visible: {JSON.stringify(!!visibility.isItemVisible(itemId))}</div>
+        <div>selected: {JSON.stringify(!!selected)}</div>
+      </div>
+      <div
+        style={{
+          height: '200px',
+        }}
+      />
+    </div>
+  );
+}
+
+
 
 export default EventsMenu;
