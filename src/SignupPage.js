4b5b92a17870d8138cd2a550d416885e762b0118
Nithish <nithishprabhu123@gmail.com>
2022-05-08 19:04:00 +0530

    Fixed some merge conflicts

--- src/SignupPage.js
+++ src/SignupPage.js
@@ -4,55 +4,32 @@ import LeafletMap from "./LeafletMap";
 import Search from "./Search";
 
 const SignupPage = () => {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [cPassword, setCPassword] = useState("");
-  const [address, setAddress] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-
-<<<<<<< HEAD
-    //Stores the name
-    const [name, setName] = useState('');
-    //Stores password
-    const [password, setPassword] = useState('');
-    //Stores confirm password
-    const [cPassword, setCPassword] = useState('');
-    //Organisation dropdown selection
-    const [type, setType] = useState('Select Emergency Organisation');
-    //Stores email
-    const [email, setEmail] = useState('');
-    //Stores phone number
-    const [phone, setPhone] = useState('');
-    //Stores the state of dropdown button
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [cPassword, setCPassword] = useState("");
+    const [address, setAddress] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
     const [state,setState]=useState(false);
-=======
-  //const [add,setAdd]=useState('');
->>>>>>> acb579b5b77f6842a78b14823a584a07c0e1be82
+    const [type,setType]=useState("Select Emergency Organisation");
 
-  const [selectPosition, setSelectPosition] = useState(null);
+    //const [add,setAdd]=useState('');
 
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
-  const handleChange = (lat, lon) => {
-    setLatitude(lat);
-    setLongitude(lon);
-    // console.log(latitude);
-  };
+    const [selectPosition, setSelectPosition] = useState(null);
 
-<<<<<<< HEAD
     const [latitude, setLatitude] = useState("");
     const [longitude, setLongitude] = useState("");
     const handleChange = (lat, lon) => {
         setLatitude(lat);
         setLongitude(lon);
         // console.log(latitude);
-    }
+    };
 
-    const handleClick=()=>{
-        if(state==false)
+
+    const handleClick = () => {
+        if (state == false)
             setState(true);
-        else if(state==true)
+        else if (state == true)
             setState(false);
     }
 
@@ -65,18 +42,18 @@ const SignupPage = () => {
 
                 <div class="organisationType">
                     <button class="dropDownButton" type="button" id="dropdownMenuButton1"
-                     data-bs-toggle="dropdown" aria-expanded="false" onClick={handleClick}>
+                        data-bs-toggle="dropdown" aria-expanded="false" onClick={handleClick}>
                         {type}
                     </button>
                     {state && <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li onClick={()=>{
-                            {/*Sets the type to police*/}
+                        <li onClick={() => {
+                            { /*Sets the type to police*/ }
                             setType("Police Station");
                             setState(false);
                         }}>Police Station</li>
                         <hr></hr>
-                        <li onClick={()=>{
-                            {/*Sets the type to hospital*/}
+                        <li onClick={() => {
+                            { /*Sets the type to hospital*/ }
                             setType("Hospital");
                             setState(false);
                         }}>Hospital</li>
@@ -88,59 +65,13 @@ const SignupPage = () => {
                     <input
                         type="text" required
                         value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
+                        onChange={(e) => setName(e.target.value)} />
                 </div>
 
                 <div className="txt_field">
                     <label><b>Location:</b></label>
                     <Search handleChange={handleChange} />
                     <LeafletMap latitude={latitude} longitude={longitude} />
-                    {/* <div
-=======
-  //Leaflet
-
-  return (
-    <div className='signup'>
-      <h2>Signup:</h2>
-      <form>
-        <div className='txt'>
-          <label>
-            <b>Police station name:</b>
-          </label>
-          <input
-            type='text'
-            required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <label>Location:</label>
-        <Search handleChange={handleChange} />
-        <LeafletMap latitude={latitude} longitude={longitude} />
-        {/* <div
->>>>>>> acb579b5b77f6842a78b14823a584a07c0e1be82
-                        style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            width: "100vw",
-                            height: "100vh",
-                        }}
-                    >
-                        <div 
-                        style={{ width: "50vw", height: "100%" }}
-                        >
-                            <LeafletMap selectPosition={selectPosition} />
-                        </div>
-                        <div 
-                        style={{ width: "50vw" }}
-                        >
-                            <Search selectPosition={selectPosition} setSelectPosition={setSelectPosition} />
-                        </div>
-                    </div> */}
-
-<<<<<<< HEAD
-
                 </div>
 
                 <div className="txt">
@@ -149,8 +80,7 @@ const SignupPage = () => {
                         type="email"
                         required
                         value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+                        onChange={(e) => setEmail(e.target.value)} />
                 </div>
 
                 <div className="txt">
@@ -159,8 +89,7 @@ const SignupPage = () => {
                         type="tel"
                         required
                         value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
-                    />
+                        onChange={(e) => setPhone(e.target.value)} />
                 </div>
 
                 <div className="txt">
@@ -169,8 +98,7 @@ const SignupPage = () => {
                         type="password"
                         required
                         value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+                        onChange={(e) => setPassword(e.target.value)} />
                 </div>
 
                 <div className="txt">
@@ -179,95 +107,23 @@ const SignupPage = () => {
                         type="password"
                         required
                         value={cPassword}
-                        onChange={(e) => setCPassword(e.target.value)}
-                    />
+                        onChange={(e) => setCPassword(e.target.value)} />
                 </div>
 
                 {/* {!isPending && <button>Add blog</button>}
-                {isPending && <button disabled>Adding</button>} */}
+    {isPending && <button disabled>Adding</button>} */}
                 <div className="loginButton">
                     <button>Signup</button>
                 </div>
 
                 <div className="signupbutton">
-                    Already have an account? <Link to="/login">Login</Link>
+                    Already have an account?<Link to="/login">Login</Link>
                     <br></br>
                     <br></br>
                 </div>
             </form>
-=======
-        <div className='txt'>
-          <label>
-            <b>Password:</b>
-          </label>
-          <input
-            type='password'
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
->>>>>>> acb579b5b77f6842a78b14823a584a07c0e1be82
-        </div>
-
-        <div className='txt'>
-          <label>
-            <b>Email:</b>
-          </label>
-          <input
-            type='email'
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <div className='txt'>
-          <label>
-            <b>Phone:</b>
-          </label>
-          <input
-            type='tel'
-            required
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-        </div>
-
-        <div className='txt'>
-          <label>
-            <b>Password:</b>
-          </label>
-          <input
-            type='password'
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <div className='txt'>
-          <label>
-            <b>Confirm Password:</b>
-          </label>
-          <input
-            type='password'
-            required
-            value={cPassword}
-            onChange={(e) => setCPassword(e.target.value)}
-          />
-        </div>
-
-        {/* {!isPending && <button>Add blog</button>}
-                {isPending && <button disabled>Adding</button>} */}
-        <div className='loginButton'>
-          <button>Signup</button>
-        </div>
-
-        <div className='signupbutton'>
-          Already have an account? <Link to='/login'>Login</Link>
-        </div>
-      </form>
-    </div>
+        
+    </div >
   );
 };
 
--- src/StartPage.js
+++ src/StartPage.js
@@ -4,7 +4,7 @@ const StartPage = () => {
     return ( 
         <div className="start">
             <h2>Login As:</h2>
-            <br></br>
+            {/* <br></br> */}
             <div className="choices">
                 <Link to="/"><button id="police"><h4>POLICE</h4></button></Link>
                 <br></br>
